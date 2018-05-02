8116b5bb5d9b3db90a2e3e091292ae2a97f0158d
Christopher Plumberg <astrophysicist87@gmail.com>
2018-05-01 21:46:09 -0500

    A few updates.

--- Persides/R4.cpp
+++ Persides/R4.cpp
@@ -15,7 +15,7 @@ void initialize_A3D_starting_values();
 
 int main(void)
 {
-	const int rmax = 50, nmax = 50, smax = 50;
+	const int rmax = 50, nmax = 50, smax = 50, tmax = 50;
 
 	double sigma_l = 1.0;
 	if (l >= 1)
@@ -31,8 +31,6 @@ int main(void)
 	
 	for (int n = 2; n <= nmax; ++n)
 	{
-		set_D3D(n);
-
 		set_B1D(n);
 		set_C1D(n);
 
@@ -74,5 +72,110 @@ void initialize_A3D_starting_values()
 		A3D[indexer_A3D(1, r, 1)]
 			= i*pow(-1.0, l+1)*pp(r-1);
 
+	return;
+}
+
+void set_B1D(int n)
+{
+	double sum = 0.0;
+	for (int u = 0; u < rmax; ++u)
+	for (int t = 0; t <= u+l; ++t)	//double check
+	for (int v = 0; v < smax; ++v)
+		sum += (l-t+u+1.0)*pp(t-u)*A3D[indexer_A3D(n-1,u,v)]*D3D(2*l+n-1,v,0);
+
+	B1D[n] = sum;
+
+	return;
+}
+
+void set_C1D(int n)
+{
+	double sum = 0.0;
+	for (int t = 0; t < tmax; ++t) //double check
+	for (int u = 0; u < rmax; ++u)
+	for (int v = 0; v < smax; ++v)
+		sum += (-l-t+u)*qp(t-u)*A3D[indexer_A3D(n-1,u,v)]*D3D(n-1-t,v,0);
+
+	C1D[n] = sum;
+
+	return;
+}
+
+void set_B3D(int n, int t, int s)
+{
+	double sum = 0.0;
+	for (int u = 0; u < rmax; ++u)
+	for (int v = 0; v < smax; ++v)
+		sum += (l-t+u+1.0)*pp(t-u)*A3D[indexer_A3D(n-1,u,v)]*D3D(2*l+n-t,v,s);
+
+	B3D[indexer_B3D(n, t, s)] = sum - B1D[n]*double(s==0)*double(2*l+n+1==t);
+
+	return;
+}
+
+void set_C3D(int n, int t, int s)
+{
+	double sum = 0.0;
+	for (int u = 0; u < rmax; ++u)
+	for (int v = 0; v < smax; ++v)
+		sum += (-l-t+u)*qp(t-u)*A3D[indexer_A3D(n-1,u,v)]*D3D(n-1-t,v,s);
+
+	C3D[indexer_C3D(n, t, s)] = sum - C1D[n]*double(s==0)*double(n==t);
+
+	return;
+}
+
+void set_A3D(int n, int r, int s)
+{
+	double sum = 0.0;
+	for (int t = 0; t <= r; ++t) //double check
+		sum += qp(r-t)*B3D[indexer_B3D(n, t, s)]
+				- pp(r-t)*C3D[indexer_C3D(n, t, s)];
+
+	A3D[indexer_A3D(n,r,s)] = 4.0*i*sum;
+}
+
+void set_Yrt()
+{
+	double prefactor = pow(-1.0,l)*Al(l);
+	for (int ix = 0; ix < n_x_pts; ++ix)
+	for (int r = 0; r < rmax; ++r)
+	for (int t = 0; t < tmax; ++t)
+	{
+		if (r<t)
+			continue;
+		double x_loc = x_pts[ix];
+		double ln_x_loc = log(x_loc);
+		complex<double> sum = 0.0;
+		for (int n = 0; n < nmax; ++n)
+		for (int s = 0; s < smax; ++s)
+		{
+			if (t>s)
+				continue;
+			double Anrst = pow(-1.0,t)*gsl_sf_choose(s, t)*A3D[indexer_A3D(n,r,s)];
+			sum += Anrst*pow(x_loc, l+n-r)*pow(ln_x_loc, s-t);
+		}
+		Yrt[indexer_Yrt(r, t, ix)] = prefactor * sum;
+	}
+
+	return;
+}
+
+void set_xinm(int n, int m)
+{
+	if (n<m)
+		return;
+	else
+	{
+		for (int ix = 0; ix < n_x_pts; ++ix)
+		{
+			complex<double> sum = 0.0;
+			for (int s = 0; s < smax; ++s)
+			{
+			
+			}
+			xinm[indexer_xinm(t, m, ix)] = conj(sum);
+		}
+	}
 	return;
 }
--- Persides/R4.h
+++ Persides/R4.h
@@ -31,12 +31,38 @@ inline double qp(int r)
 		return (0.0);
 	else
 	{
-		double twor_fact = 2.0*gsl_sf_fact(r);
-		double twolprp1_fact = gsl_sf_fact(r);
-		double lpr_fact = gsl_sf_fact(r);
+		double r_fact = gsl_sf_fact(r);
+		double twolprp1_fact = gsl_sf_fact(2*l+r+1);
+		double lpr_fact = gsl_sf_fact(l+r);
 
-		return ( pow(-1.0, l+1)*lpr_fact*lpr_fact / ( twor_fact*twolprp1_fact ) );
+		return ( pow(-1.0, l+1)*lpr_fact*lpr_fact / ( 2.0*r_fact*twolprp1_fact ) );
 	}
+}
+
+inline double Al(int il)
+{
+	double l_fact = gsl_sf_fact(l);
+	double twol_fact = gsl_sf_fact(2*l);
+	double twolpone_fact = gsl_sf_fact(2*l+1);
+
+	double num = pow(-2.0,l)*pow(l_fact, 3.0);
+	double den = twol_fact*twolpone_fact;
+
+	return ( num / den );
+}
+
+inline double D3D(int r, int s, int t)	//N.B. - not the same as r, s, t elsewhere!
+{
+	if ( r != -1 and 0 <= t < s+1 )
+	{
+		double num = pow(-1.0, s-t)*gsl_sf_fact(s);
+		double den = pow(r+1.0, s-t+1.0)*gsl_sf_fact(t);
+		return (num/den);
+	}
+	else if ( r == -1 and t == s+1 )
+		return ( 1.0 / (s + 1.0) );
+	else
+		return (0.0);
 }
 
 inline int indexer_A3D(int n, int r, int s)
